971e72c4e6c76bf93df35d346bade42ccb508a8f
Alexey Bolgov <bolgov.alex.s@gmail.com>
2020-04-12 17:48:12 +0300

    test commit

--- app/src/main/java/ru/lbas/memes/SplashActivity.kt
+++ app/src/main/java/ru/lbas/memes/SplashActivity.kt
@@ -1,11 +1,19 @@
 package ru.lbas.memes
 
 import android.os.Bundle
+import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
+import java.lang.Thread.sleep
+import java.util.*
+import kotlin.concurrent.thread
 
 class SplashActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        sleep(300)
+        setContentView(R.layout.activity_login)
     }
+
+
 }
