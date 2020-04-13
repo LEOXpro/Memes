b77f1fe7157e2fd4a659da8db8281ebfb896ed59
Alexey Bolgov <bolgov.alex.s@gmail.com>
2020-04-13 22:01:33 +0300

    add activity login

--- app/src/main/java/ru/lbas/memes/LoginActivity.kt
+++ app/src/main/java/ru/lbas/memes/LoginActivity.kt
@@ -1,5 +1,6 @@
 package ru.lbas.memes
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
--- app/src/main/java/ru/lbas/memes/SplashActivity.kt
+++ app/src/main/java/ru/lbas/memes/SplashActivity.kt
@@ -1,19 +1,15 @@
 package ru.lbas.memes
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
-import java.lang.Thread.sleep
-import java.util.*
-import kotlin.concurrent.thread
 
 class SplashActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        sleep(300)
-        setContentView(R.layout.activity_login)
+        Handler().postDelayed({ var SecAct: Intent = Intent(getApplicationContext(), LoginActivity::class.java)
+            startActivity(SecAct) }, 300)
     }
-
-
 }
